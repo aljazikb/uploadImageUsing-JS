d78fea0eb41f0134c3d8b0a9f018e1bd928aefb0
ALJAZI <aljazi.kb@gmail.com>
2026-01-16 19:31:34 +0300

    Add files via upload

--- Test.js
+++ Test.js
@@ -1,8 +1,26 @@
 document.getElementById("input").addEventListener("change",function(){
     console.log("clicked")
 
-    let img=document.getElementById("image")
-    img.src=URL.createObjectURL(this.files[0])
+    const file = this.files[0]
+    const reader=new FileReader()
+
+    reader.onload =  function(){
+        const base64Image = reader.result;
+
+        document.getElementById("image").src=base64Image 
+        localStorage.setItem("image",base64Image )
+
+    };
+     reader.readAsDataURL(file); // converts image to Base64
 
 
 })
+
+
+window.addEventListener("load",function(){
+    const storedImage = localStorage.getItem("image")
+    if (storedImage) {
+        document.getElementById("image").src = storedImage;
+    }
+});
+
